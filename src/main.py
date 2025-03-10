9f934399bbf9abc5a4b806f109de06fe16ea3fde
KuBaN658 <garkovski_dmitri@mail.ru>
2025-03-10 09:23:30 +0300

    lesson 5 группировка тестов и исключения

--- src/main.py
+++ src/main.py
@@ -3,9 +3,16 @@ from typing import Union
 
 class Calculator:
     def divide(self, x: Union[int, float], y: Union[int, float]) -> Union[int, float]:
+        if not isinstance(x, (int, float)) or not isinstance(y, (int, float)):
+            raise TypeError('x and y must be numeric')
+        if y == 0:
+            raise ZeroDivisionError('division by zero')
+        
         return x / y
     
     def add(self, x: Union[int, float], y: Union[int, float]) -> Union[int, float]:
+        if not isinstance(x, (int, float)) or not isinstance(y, (int, float)):
+            raise TypeError('x and y must be numeric')
         return x + y
     
 
